b3c1966363af57658fc477851d486d29155ae2d9
LTisme <284618025@qq.com>
2021-01-17 18:43:06 +0800

    重构了一下往场站信息里添加设备的函数，已测试通过，可以正常执行

--- common/dispose_of_request.py
+++ common/dispose_of_request.py
@@ -9,6 +9,7 @@ import os
 import json
 from api_format import ContentTypeDisposition, Data, Headers
 import api_functions
+import api_relogic  # 重构了逻辑函数
 import logging
 import pprint
 import requests
@@ -35,7 +36,7 @@ with open('data.json', encoding='UTF-8') as fbj:
 
 if __name__ == '__main__':
     headers = Headers()
-    body = Data()   # 这个示例里包含每个特定api操作的要求格式
+    body = Data()   # 这个实例里包含每个特定api操作的要求格式
 
     # TODO: 先post请求getalldevicestype以获得内容，再用厂家名、CT、PT正则匹配得出结果
     getalldevicetypes = ContentTypeDisposition(headers_=headers.headers_query_templates)  # 这个请求不需要带请求体
@@ -68,56 +69,8 @@ if __name__ == '__main__':
         st = exp.request_method_edge_getallboxinfo()     # 获得Edge中的盒子信息
         if len(st.json()['data']) == 0:
             print('该场站下没有盒子')
-            # 去往场站信息中添加设备，并获得每个新添加的设备的objectID
-            # 需要先请求一次childdetail来获得新旧设备以防止残留设备与已有设备名字重名带来的objectID无法区分
-            childdetail = ContentTypeDisposition(body.station_childdetail(siteID), headers.headers_multipart)
-            childdetail_res = childdetail.request_method_stationdetail_childdetail()
-            projid = childdetail_res.json()['data'][0]['objectID']  # 获得重要的projid
-            catid = childdetail_res.json()['data'][0]['categoryIDs'][0]
-            print('projid is ' + projid)
-            # TODO: 1请求childdetailpagestep
-            childdetailpagestep = ContentTypeDisposition(body.station_childdetailpagestep(projid), headers.headers_multipart)
-            childdetailpagestep_res = childdetailpagestep.request_method_stationdetail_childdetailpagestep()
-            if len(childdetailpagestep_res.json()['data']['data']) == 0:
-                # TODO: 1.1若设备数为零，说明没有残留设备，创建一个空列表，每adddevice后，post一次childdetailpagestep,将获得的新objectID放进空列表中
-                print(' 这个场站下没有残留设备')
-                old_list = []
-            else:
-                # TODO: 1.2若设备数不零，说明有残留设备，创建一个空列表，将已有设备的objectID放入列表，
-                #  然后每adddevice后post一次childdetailpagestep并与旧列表比对，将新ID作为新添设备的额外属性
-                print(' 这个场站没有盒子但有残留设备，请检查是否需要删除')
-                old_list = []
-                for val in childdetailpagestep_res.json()['data']['data']:
-                    old_list.append(val['objectID'])
-                print('oldlist is ', old_list)
-            new_list = []   # 这个列表用来存储新增设备的各个属性，这样就绝对不会因为重名搞混objectID，已测试通过
-            for each_device in each_station['信息']:
-                PORT_NUM = str(each_device['port_num'])  # 获得了端口号
-                KitName = str(each_device['device_name'])  # 设备名
-                Capacity = str(each_device['capacity'])  # 容量
-                RatedCurrent = str(each_device['rated_current'])  # 额定电流
-                DEVICE_CT = str(each_device['CT'])  # CT
-                DEVICE_PT = str(each_device['PT'])  # PT
-                DEVICE_MANUFACTURER = str(each_device['manufacturer'])
-                LOGIC_NUM = str(each_device['modbus'])  # 获得了对应的公共地址，即逻辑编号
-                adddevice = ContentTypeDisposition(body.station_adddevice(projid, catid, KitName, RatedCurrent,
-                                                                          Capacity, siteID), headers.headers_multipart)
-                adddevice_res = adddevice.request_method_stationdetail_adddevice()
-                # 每次添加一个设备需要post一次childdetailpagestep以获得设备对应的objectID
-                childdetailpagestep_res = childdetailpagestep.request_method_stationdetail_childdetailpagestep()
-                for val in childdetailpagestep_res.json()['data']['data']:
-                    if val['objectID'] not in old_list:     # 找到新增设备的objectID
-                        ip, sn = api_functions.judgement(PORT_NUM)
-                        new_list.append({'name': KitName, 'objectID': val['objectID'], 'CT': DEVICE_CT, 'PT': DEVICE_PT,
-                                         'modbus': LOGIC_NUM, 'manufacturer': DEVICE_MANUFACTURER, 'portnum': PORT_NUM,
-                                         'ip': ip, 'sn': sn})
-                        # 将objectID与后续必要信息其记录到new_list中
-                        old_list.append(val['objectID'])    # 并将它放进old_list中
-                if adddevice_res.json()['data'] is True:    # 这对ifelse可以注释掉或者删掉
-                    print('添加设备成功！')
-                else:
-                    print('添加设备失败！')
-            pprint.pprint(new_list)
+            # 往场站信息里添加盒子
+            new_list = api_relogic.add_devices_in_station_info(siteID, each_station, body, headers, logging)
 
             # TODO: 去到Edge接入中新建盒子、新建连接（连接名字从104转发1开始算）、选设备及模板、最后点击发布
             # 用set的办法来将port_list里删除重复项
@@ -209,13 +162,16 @@ if __name__ == '__main__':
 
         else:
             print('该场站下有盒子')
+            # TODO: 首先getallboxinfo，因为有盒子了嘛，然后去找其下的端口号——注意端口号只于连接有关，与盒子无关，故是去判断连接！！！
+            if len(aim['data'][0]['collectType']) == 0
+
             # 需要先请求一次childdetail来获得新旧设备以防止残留设备与已有设备名字重名带来的objectID无法区分
             childdetail = ContentTypeDisposition(body.station_childdetail(siteID), headers.headers_multipart)
             childdetail_res = childdetail.request_method_stationdetail_childdetail()
             projid = childdetail_res.json()['data'][0]['objectID']  # 获得重要的projid
             catid = childdetail_res.json()['data'][0]['categoryIDs'][0]
             print('projid is ' + projid)
-            # TODO: 1遍历所有盒子查看盒子下有无对应的端口号，若有则记录到日志中，让人手动去查具体情况；
+            # TODO: 1遍历所有连接查看盒子下有无对应的端口号，若有则记录到日志中，让人手动去查具体情况；
 
             # TODO: 2遍历所有盒子查看盒子下有无对应的端口号，若无则去往场站信息中添加设备，并获得每个新添加的设备的objectID
 
